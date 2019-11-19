0623bd22d14560919c70b0e2cbbe3eee1b09a183
Daiane Silva <adm.daianesilva@gmail.com>
2019-11-19 07:59:03 -0300

    Carousel teacher

--- src/components/CarouselTeachers/style.ts
+++ src/components/CarouselTeachers/style.ts
@@ -4,11 +4,11 @@ export const Container = styled.div`
   margin-top: 30px;
   position: relative;
   .slick-slide {
+    background: #e2dcdc;
     transform: scale(1);
-    transition: all 1s ease;
-    background: #e3e3e3;
+    transition: all 0.6s ease;
     box-shadow: inset 0px 0px 0px 5px rgba(255, 255, 255, 1);
-    opacity: 0.5;
+    opacity: 0.6;
 
     @media (max-width: 768px) {
       height: 338px;
@@ -18,11 +18,11 @@ export const Container = styled.div`
     }
   }
   .slick-current {
-    z-index: 4;
     background: #fff !important;
-    opacity: 1 !important;
+    z-index: 1050;
     box-shadow: 1px 1px 13px 0px rgba(0, 0, 0, 0.5) !important;
-    transition: all 1s ease;
+    opacity: 1 !important;
+    transition: all 0.6s ease;
   }
 `;
 
